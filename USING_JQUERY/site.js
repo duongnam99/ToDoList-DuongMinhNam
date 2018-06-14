ea8ddbfd681fec0acf53cd12574b1503c73ec08e
duongnam99 <duongminhnam99@gmail.com>
2018-06-14 13:57:30 +0700

    up

---
+++ USING_JQUERY/site.js
@@ -0,0 +1,158 @@
+﻿$(function(){
+
+ 	var number_job = $('li').length;
+ 	var number_complete;
+ 	var number_active = number_job;
+
+ 	function click_add(){
+		var Title = $('#myInput').val();
+ 		if(Title != ''){
+ 			$('ul').append('<li>'+Title+'</li>');
+	 		$('.xoa').append('<button class="nutxoa"><i class="fa fa-remove"></i></button>');
+	 		$('#myInput').val('');
+
+	 		number_job += 1; // after add, number_job + 1
+			$('.number_job').text(number_job);
+			number_active +=1;
+			$('.number_active').text(number_active);
+ 		}	
+	}	
+	// catch enter keyboard event
+	$('#myInput').keypress(function(e) {
+	    if(e.keyCode == 13) {
+	        click_add();
+	        return false; // after add job, return false to stop reload page
+	    }
+	});
+	
+ 	// Handle add job event
+ 	$('html').on('click', '.add', function(event) {
+ 		click_add();
+ 	});
+
+ 	// Handle check job event!
+ 	$('html').on('click', 'ul li', function(event) {
+ 		// show color
+ 		$(this).toggleClass('checked');
+
+ 		// show button "clear completed" when checking li element
+ 		if($('ul li').hasClass('checked')){
+ 			$('.clear').removeClass('hide_clear_button');
+ 		}else{
+ 			$('.clear').addClass('hide_clear_button');
+ 		}	
+
+ 		number_complete = $('.checked').length;	
+ 		$('.number_complete').text(number_complete);
+
+ 		number_active = number_job - number_complete;
+ 		$('.number_active').text(number_active);
+ 	});
+ 	// Handle delete job event
+ 	$('html').on('click', '.nutxoa', function(event) {
+ 		// get "li" index
+		var index = $(this).index()+1;
+		// number of job:
+		number_job -= 1;
+		$('.number_job').text('');
+		$('.number_job').text(number_job);
+		//active and compele (if "li" element has checked and deleted => number_complete -1)
+		if($('ul li:nth-child('+index+')').hasClass('checked')){
+			number_complete -= 1; 
+	
+			$('.number_complete').text(number_complete);
+			$(this).remove();
+			$('ul li:nth-child('+index+')').remove();
+		}else{
+			number_active -=1
+	
+			$('.number_active').text(number_active);
+			$(this).remove();
+			$('ul li:nth-child('+index+')').remove();		
+		}
+ 	});
+ 	
+ 	//complete
+ 	$('html').on('click', '.complete', function(event) {
+
+ 		$('.button').removeClass('button_color');
+ 		$(this).addClass('button_color');
+ 		
+ 		$('ul li, .nutxoa').hide();
+ 		for (var i = 0; i <=$('li').length; i++) {
+ 			if($('ul li:nth-child('+i+')').hasClass('checked')){
+ 				$('ul li:nth-child('+i+'), .nutxoa:nth-child('+i+')').show();
+ 			}
+ 		}
+ 	});
+ 	//active
+ 	$('html').on('click', '.active', function(event) {
+
+ 		$('.button').removeClass('button_color');
+ 		$(this).addClass('button_color');
+
+ 		$('ul li, .nutxoa').hide();
+ 		for (var i = 1; i <= $('li').length; i++) {
+ 			if( !$('ul li:nth-child('+i+')').hasClass('checked')){
+ 				$('ul li:nth-child('+i+'), .nutxoa:nth-child('+i+')').show();
+ 			}
+ 		}	
+ 	});
+ 	// all
+ 	$('html').on('click', '.all', function(event) {
+ 		//show color
+ 		$('.button').removeClass('button_color');
+ 		$(this).addClass('button_color');
+ 		$('ul li, .nutxoa').show();
+ 	});
+ 	// clear completed 
+ 	$('html').on('click', '.clear', function(event) {
+ 		$('.button').removeClass('button_color');
+ 		$('.all').addClass('button_color');
+
+ 		number_job -= number_complete;
+ 		number_complete = 0;
+
+ 		$('.number_job').text(number_job);
+ 		$('.number_complete').text(number_complete);
+ 		var i = $('li').length;
+ 		// for (var i = 1; i <= $('li').length; i++) {
+ 		while( i <= $('li').length && i >= 0){
+ 			if( $('ul li:nth-child('+i+')').hasClass('checked')){
+ 				$('ul li:nth-child('+i+'), .nutxoa:nth-child('+i+')').remove(); //after remove, length change !!!
+ 			}
+ 			i--;
+ 		}	
+ 		$('ul li, .nutxoa').show();
+ 	});
+ 	//CHECK ALL
+	var clicks = 0;
+	$('html').on('click', '.check_all', function(event){
+		$('.clear').removeClass('hide_clear_button');
+
+		$('.button').removeClass('button_color');
+ 		$('.all').addClass('button_color');
+ 		//CHECK ALL
+ 		clicks += 1;
+ 		if( clicks %2 == 1){
+	 		for (var i = 1; i <= $('li').length ; i++) {
+	 			$('ul li:nth-child('+i+')').addClass('checked');
+	 		}
+	 		if($('ul li').hasClass('checked')){
+	 			$('.clear').removeClass('hide_clear_button');
+	 		}else{
+	 			$('.clear').addClass('hide_clear_button');
+ 			}	
+ 		}else{
+ 			for (var i = 1; i <= $('li').length ; i++) {
+	 			$('ul li:nth-child('+i+')').removeClass('checked');
+	 		}
+	 		if($('ul li').hasClass('checked')){
+	 			$('.clear').removeClass('hide_clear_button');
+	 		}else{
+	 			$('.clear').addClass('hide_clear_button');
+ 			}	
+ 		}
+	});
+})  
+
---
+++ USING_JS/site.js
@@ -0,0 +1,176 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+	var number_job = document.querySelectorAll('li').length;
+ 	var number_complete = document.querySelectorAll('li').length;
+ 	var number_active = number_job;
+
+ 	function click_add(){
+ 		var Title = document.getElementById("myInput").value;
+ 		if(Title != ''){
+ 			var div = document.createElement('div');
+ 			document.querySelector('ul').appendChild(div);
+ 			div.classList.add('row','job');
+ 			div.innerHTML = "<li class='col-sm-11'>"+ Title +"</li><button class='col-sm-1 nutxoa'>x</button>"
+	 		
+	 		document.getElementById('myInput').value = '';
+
+	 		number_job += 1; // after add, number_job, number_active + 1
+	 	 	document.querySelector('.number_job').textContent = number_job; 	
+			number_active +=1;
+	 	 	document.querySelector('.number_active').textContent = number_active;
+ 		}
+ 	}
+ 	//handle add event
+ 	document.querySelector('.add').addEventListener('click', function(){
+ 		click_add();
+ 	})
+ 	// TRIGGER ENTER INPUT TEXT
+ 	var input = document.getElementById("myInput");
+	input.addEventListener("keypress", function(event) {
+		if (event.keyCode == 13) {
+	       	click_add();
+	       	return false;
+	   	}
+	});
+
+
+ 	// FUNCTION CHECK CLASS IN LIST ELEMENT
+ 	function hasClass(element, className) {
+ 		var a = 0;
+ 		for (var i = 0; i < element.length; i++) {
+ 			if((' ' + element[i].className + ' ').indexOf(' ' + className+ ' ') > -1){ // classes split by space: ' '
+ 				a += 1;
+ 			} 
+ 		}
+ 		if(a == 0){ 
+ 			return false;
+ 		}else{
+ 			return true;
+ 		}
+	}
+	function show_clear_button(){
+		if(hasClass(document.querySelectorAll('li'), 'checked') == true){
+   			document.querySelector('.clear').classList.remove('hide_clear_button');
+  		}else if(hasClass(document.querySelectorAll('li'), 'checked') == false){
+  			document.querySelector('.clear').classList.add('hide_clear_button');
+  		}
+	}
+ 	//HANDLE CLICK 'li' ELEMENT EVENT
+ 	var list = document.querySelector('ul');
+	list.addEventListener('click', function(ev) {
+  	if (ev.target.tagName === 'LI') {
+  		// show "li" color
+  		ev.target.classList.toggle('checked');
+  		//show clear button:
+  		show_clear_button()
+   		
+		//number of compeleted job
+ 		var number_complete = document.querySelectorAll('.checked').length;		
+ 	 	document.querySelector('.number_complete').textContent = number_complete;
+ 		// // number of active job
+ 		number_active = number_job - number_complete;
+ 	 	document.querySelector('.number_active').textContent = number_active;
+ 	 }
+	});
+	// DELETE JOB
+ 	var deletebutton = document.querySelector('ul'); //select parent, addEventListener: children
+ 	deletebutton.addEventListener('click', function(ev){
+ 		if(ev.target.tagName === 'BUTTON'){
+ 			//delete job, number_job -1
+ 			ev.target.parentElement.remove();
+ 			number_job -= 1;
+ 	 		document.querySelector('.number_job').textContent = number_job;
+ 		}
+ 	})
+
+ 	function remove_all_button_color(){
+ 		var button = document.querySelectorAll('.button');
+ 		for (var i = 0; i < button.length; i++) {
+ 			button[i].classList.remove('button_color');
+ 		}
+ 	}
+ 	function complete(element, className) {
+ 		var a = 0;
+ 		for (var i = 0; i < element.length; i++) {
+ 			if((' ' + element[i].className + ' ').indexOf(' ' + className+ ' ') > -1){ // classes split by space: ' '
+ 				element[i].parentElement.classList.remove('hide'); // show div checked
+ 			} 
+ 		}	
+	}
+ 	function active(element, className) {
+ 		var a = 0;
+ 		for (var i = 0; i < element.length; i++) {
+ 			if((' ' + element[i].className + ' ').indexOf(' ' + className+ ' ') > -1){ // classes split by space: ' '
+ 				element[i].parentElement.classList.add('hide'); // hide 
+ 			} 
+ 		}	
+	}
+ 	function clear(element, className) {
+ 		var i = element.length -1;
+ 		while(i <= element.length -1 && i >=0){
+ 			if((' ' + element[i].className + ' ').indexOf(' ' + className + ' ') > -1){ // classes split by space: ' '
+ 				console.log('nam');
+ 				document.querySelector('ul').removeChild(element[i].parentElement); 
+ 			} 
+ 			i--;
+ 		}	
+	}
+ 	function hide(element) {
+ 		var a = 0;
+ 		for (var i = 0; i < element.length; i++) {
+ 			element[i].classList.add('hide');
+ 		}
+	}
+ 	function show(element) {
+ 		var a = 0;
+ 		for (var i = 0; i < element.length; i++) {
+ 			element[i].classList.remove('hide');
+ 		}
+	}
+ 	document.querySelector('.active').addEventListener('click', function(){
+ 		remove_all_button_color();
+ 		this.classList.add('button_color');
+ 		// show all and hide checked job
+ 		show(document.querySelectorAll('.row.job'));
+ 		active(document.querySelectorAll('li'), 'checked');
+ 	}) 
+	document.querySelector('.all').addEventListener('click', function(){
+ 		remove_all_button_color();
+ 		this.classList.add('button_color');
+ 		// show all
+ 		show(document.querySelectorAll('.row.job'));
+ 	}) 
+	document.querySelector('.complete').addEventListener('click', function(){
+ 		remove_all_button_color();
+ 		this.classList.add('button_color');
+ 		// hide all and show checked job
+ 		hide(document.querySelectorAll('.row.job'));
+ 		complete(document.querySelectorAll('li'), 'checked');
+ 	}) 
+	document.querySelector('.clear').addEventListener('click', function(){
+		remove_all_button_color();
+ 		document.querySelector('.all').classList.add('button_color');
+
+ 		clear(document.querySelectorAll('li'), 'checked');
+
+	})
+	//CHECK ALL
+	var clicks = 0;
+	document.querySelector('.check_all').addEventListener('click', function(){
+		remove_all_button_color();
+ 		document.querySelector('.all').classList.add('button_color');
+ 		//CHECK ALL
+ 		clicks += 1;
+ 		var li = document.querySelectorAll('li');
+ 		if( clicks %2 == 1){
+	 		for (var i = 0; i < li.length ; i++) {
+	 			li[i].classList.add('checked');
+	 		}
+	 		show_clear_button();
+ 		}else{
+ 			for (var i = 0; i < li.length ; i++) {
+	 			li[i].classList.remove('checked');
+	 		}
+	 		show_clear_button();
+ 		}
+	})
+},false)
